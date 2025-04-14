8ce80275594285e593116481b3786227f9b588d7
XamiD <130274873+sogdian-boy@users.noreply.github.com>
2025-04-14 15:15:42 +0500

    Add files via upload

---
+++ prKol_ind3_isomatov_h_v1/prKol_ind3_Isomatov_h_v3/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+namespace prKol_ind3_Isomatov_h_v3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector a;
+            Vector b;
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("\nВведите название файла(без расширения):");
+                    string file = Console.ReadLine()+".txt";
+                    if (File.Exists(file))
+                    {
+                        string[] Coordinates = File.ReadAllLines(file);
+                        bool correctB = true;
+                        string coorA = Coordinates[0];
+                        bool correct = Vector.isCoordinate(coorA);
+                        if (correct)
+                        {
+                            a = new Vector(coorA);
+                            Console.WriteLine($"Вектор а = {a.FullVectorCoordinate()}");
+                            string coorB = Coordinates[1];
+                            correctB = Vector.isCoordinate(coorB);
+                            if (correctB)
+                            {
+                                b = new Vector(coorB);
+                                Console.WriteLine($"Вектор b = {b.FullVectorCoordinate()}");
+                                bool ON = true;
+                                while (ON)
+                                {
+                                    Console.WriteLine($"\na = {a.FullVectorCoordinate()}\nb = {b.FullVectorCoordinate()}\nВыберите действие\n1. Сложить вектора\n2. Вычитание векторов\n3. Скалярное произведение векторов\n4. Длины векторов\n5. Косинус угла между векторами \n\n6. Выход");
+                                    int choice = int.Parse(Console.ReadLine());
+                                    switch (choice)
+                                    {
+                                        case 1: Console.WriteLine($"Результат сложения = " + (a.Plus(b).FullVectorCoordinate())); break;
+                                        case 2: Console.WriteLine($"Результат вычитания = " + (a.Minus(b).FullVectorCoordinate())); break;
+                                        case 3: Console.WriteLine($"Скалярное произведение векторов = " + (a.Multiply(b))); break;
+                                        case 4: Console.WriteLine($"Длины векторов:  \na = {a.Length()}\nb = {b.Length()}"); break;
+                                        case 5: Console.WriteLine($"Косинус угла между векторами = {a.Multiply(b) / (a.Length() * b.Length())}"); break;
+                                        case 6: Console.Clear(); ON = false; break;
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Ошибка: координата задана неправильно");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Ошибка: координата задана неправильно");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ошибка: Такого файла не существует!");
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Ошибка: Неверный формат данных");
+                }
+            }
+        }
+    }
+}
---
+++ prKol_ind3_isomatov_h_v1/prKol_ind3_Isomatov_h_v3/Vector.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prKol_ind3_Isomatov_h_v3
+{
+    public class Vector
+    {
+        double x { get; set; }
+        double y { get; set; }
+        double z { get; set; }
+
+        public Vector(double A, double B, double C)
+        {
+            x = A;
+            y = B;
+            z = C;
+            
+        }
+        public Vector(string coordinate)
+        {
+            string[] coor = (coordinate.Split('.'));
+            x = Convert.ToDouble(coor[0]);
+            y = Convert.ToDouble(coor[1]);
+            z = Convert.ToDouble(coor[2]);
+        }
+        public static bool isCoordinate(string coor)
+        {
+            bool correct = true;
+            if (coor != "" && coor.IndexOf('.') != 0 && coor.IndexOf('.') != coor.Length - 1)
+            {
+                for (int i = 1; i < coor.Length; i++)
+                {
+                    if (coor[i - 1] == '.' && coor[i] == '.')
+                    {
+                        correct = false;
+                        break;
+                    }
+                }
+                if (correct)
+                {
+                    return true;
+                }
+                else
+                    return false;
+
+            }
+            else
+                return false;
+        }
+            public Vector Plus(Vector vectB)
+        {
+            double A = x + vectB.x;
+            double B = y + vectB.y;
+            double C = z + vectB.z;
+            return new Vector(A, B, C);
+        }
+        public Vector Minus(Vector vectB)
+        {
+            double A = x - vectB.x;
+            double B = y - vectB.y;
+            double C = z - vectB.z;
+            return new Vector(A,B,C);
+        }
+        public double Multiply(Vector B)
+        {
+            double A = x * B.x + y * B.y + z * B.z;
+            return A;
+        }
+        public double Length()
+        {
+            double length = Math.Sqrt(Math.Pow(x,2)+ Math.Pow(y, 2)+ Math.Pow(z, 2));
+            return Math.Abs(length);
+        }
+        public string FullVectorCoordinate()
+        {
+            return $"{x}.{y}.{z}";
+        }
+
+    }
+}
